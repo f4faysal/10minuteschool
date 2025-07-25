a1ab4234e1ff8f4c7896a50ac2decd9af95976c8
f4faysal <f4faysals@gmail>
2025-07-26 01:43:46 +0600

    feat(main): add initial UI components and utility functions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
